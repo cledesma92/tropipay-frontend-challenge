70ca9c608b102dd4cd0be9f92fa75bb294dcfc80
Christian Ledesma <cledesma92@outlook.com>
2022-10-19 03:24:10 -0300

    Se creó el componente 'Results'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import { SearchBar } from "./components/SearchBar"
+import { Results } from "./components/Results"
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <h1>FronEnd Challenge</h1>
-        <SearchBar></SearchBar>
+        <SearchBar/>
+        <Results/> 
       </React.Fragment>
     );
   }
---
+++ src/components/Results.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Results = () => {
+    return <h2>Resultados</h2>
+}
+
+export {Results}
